5905f85804b31b921555219c0f5e75b093f6b306
nancho2602 <nancho2602@gmail.com>
2025-06-04 22:38:53 -0600

    initial code from Codex, working on xCode

---
+++ CancerTracker/CancerTracker/Models/EventStore.swift
@@ -0,0 +1,15 @@
+
+import Foundation
+
+class EventStore: ObservableObject {
+    @Published var events: [Event] = []
+
+    func add(_ event: Event) {
+        events.append(event)
+    }
+
+    func update(_ event: Event) {
+        guard let index = events.firstIndex(where: { $0.id == event.id }) else { return }
+        events[index] = event
+    }
+}
